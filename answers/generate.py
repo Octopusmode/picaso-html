d9f22961752d4829674fd960e5b497b68e10783e
Maksim Osminin <osmininmaks52@gmail.com>
2023-03-21 02:28:42 +0300

    Добавляю фон

--- answers/generate.py
+++ answers/generate.py
@@ -4,7 +4,7 @@ import json
 hobbies = []
 jobs = []
 
-with open('answers.csv', newline='', encoding='utf-8') as csvfile:
+with open('answers/answers.csv', newline='', encoding='utf-8') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         if row['hobby']:
@@ -15,8 +15,8 @@ with open('answers.csv', newline='', encoding='utf-8') as csvfile:
 tags1 = [{"name": hobby, "link": "fake-link"} for hobby in hobbies]
 tags2 = [{"name": job, "link": "fake-link"} for job in jobs]
 
-with open('tags1.json', 'w', encoding='utf-8') as outfile:
-    json.dump(tags1, outfile, indent=4, ensure_ascii=False, separators=(',', ':'))
-
 with open('tags2.json', 'w', encoding='utf-8') as outfile:
+    json.dump(tags1, outfile, indent=4, ensure_ascii=False, separators=(',', ':'))
+    
+with open('tags1.json', 'w', encoding='utf-8') as outfile:
     json.dump(tags2, outfile, indent=4, ensure_ascii=False, separators=(',', ':'))
